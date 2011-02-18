63d435d3dc0100b88dcb8a0a6c588e985b3d4366
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-02-18 15:22:27 +0200

    Finished getDevices()

--- midinode.cc
+++ midinode.cc
@@ -13,6 +13,15 @@ A NodeJS module for access to native MIDI drivers.
 	obj->Set(	v8::String::NewSymbol(name),				\
 			v8::FunctionTemplate::New(callback)->GetFunction())
 
+#define SET_VALUE(obj, name, value, type)					\
+	obj->Set(	v8::String::NewSymbol(name),				\
+			v8::type::New(value))
+
+#define MIDI_DEVICE_INPUT	0
+#define MIDI_DEVICE_OUTPUT	1
+
+v8::Handle<v8::Object> midiDevice(int id);
+
 v8::Handle<v8::Value> getDevices(const v8::Arguments& args);
 
 extern "C" void
@@ -23,5 +32,24 @@ init (v8::Handle<v8::Object> target)
 }
 
 v8::Handle<v8::Value> getDevices(const v8::Arguments& args){
-	return v8::Integer::New(16);
+	int devCount = Pm_CountDevices();
+	v8::Handle<v8::Array> devices = v8::Array::New(devCount);
+	int i;
+	for (i=0; i<devCount; i++){
+		devices->Set(i, midiDevice(i));
+	}
+	return devices;
+}
+
+v8::Handle<v8::Object> midiDevice(int id){
+	const PmDeviceInfo* devInfo = Pm_GetDeviceInfo(id);
+	v8::Handle<v8::Object> devObj = v8::Object::New();
+	SET_VALUE(devObj, "name", devInfo->name, String);
+	SET_VALUE(devObj, "id", id, Integer);
+	if (devInfo->input){
+		SET_VALUE(devObj, "type", "input", String);
+	} else if (devInfo->output){
+		SET_VALUE(devObj, "type", "output", String);
+	}
+	return devObj;
 }
